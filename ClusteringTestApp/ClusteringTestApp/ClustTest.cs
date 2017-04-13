4599b48f74e41a80587665c5ba99270f91f222fd
Medved145 <bacardiparkhomencko@mail.ru>
2017-04-14 02:21:22 +0600

    Add files

---
+++ ClusteringTestApp/ClusteringTestApp/ClustTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClusteringTestApp
+{
+    class ClustTest
+    {
+        public string[] ClusteringTestApp(double[] data)
+        {
+            double r = -1;//r* по формуле
+            double ra = 0;//r1 по формуле
+            double[] F = new double[data.Length];
+            string[] result;
+            for (int i = 0; i < data.Length; i++)
+            {
+                for (int j = 0; j < data.Length; j++)
+                {
+                    if (i != j)
+                    {
+                        ra = Math.Sqrt(Math.Pow((data[i] - data[j]), 2) + Math.Pow(r, 2));
+                        F[i] += 1-(ra/(ra-r));
+                    }
+                    else
+                    {
+                        F[i] += 1;
+                    }
+                }
+                F[i] /= data.Length;
+            }
+            result = new string[data.Length];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                result[i] = data[i].ToString() + ", " + F[i].ToString();
+            }
+            return result;
+        }
+
+        public string[] ClusteringTestApp(double[][] data)
+        {
+            double r = -1;//r* по формуле
+            double ra = 0;//r1 по формуле
+            double[] F = new double[data.Length];
+            string[] result;
+            for (int i = 0; i < data.Length; i++)
+            {
+                for (int j = 0; j < data.Length; j++)
+                {
+                    if (i != j)
+                    {
+                        ra = Math.Sqrt(Math.Pow((data[i][0] - data[j][0]), 2) + Math.Pow((data[i][1] - data[j][1]), 2) + Math.Pow(r, 2));
+                        F[i] += 1 - (ra / (ra - r));
+                    }
+                    else
+                    {
+                        F[i] += 1;
+                    }
+                }
+                F[i] /= data.Length;
+            }
+            result = new string[data.Length];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                result[i] = data[i][0].ToString() + ";" + data[i][1] + ", " + F[i].ToString();
+            }
+            return result;
+        }
+    }
+}
---
+++ ClusteringTestApp/ClusteringTestApp/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClusteringTestApp
+{
+    public partial class Form1 : Form
+    {
+        private int count = 0;
+        private string massive = String.Empty;
+        private double[] dmassive;
+        private double[][] ddmassive;
+        ClustTest clusterist = new ClustTest();
+        public Form1()
+        {
+            InitializeComponent();
+            ConsoleTextBox.Text += "Введите количество точек";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            doubleMass();
+            //double[][] ddd = new double[5][];
+            //for (int i = 0; i < ddd.Length; i++)
+            //{
+            //    ddd[i] = new double[2];
+            //}
+            //ConsoleTextBox.Text += Environment.NewLine;
+            //ConsoleTextBox.Text += ddd.Length.ToString();
+
+        }
+
+        private void doubleMass()
+        {
+            if (massive == String.Empty && count != 0)
+            {
+                string[] splitmassive;
+
+                ConsoleTextBox.Text += Environment.NewLine;
+                ConsoleTextBox.Text += InputTextBox.Text;
+
+                massive = InputTextBox.Text;
+                massive = massive.Replace('.', ',');
+
+                splitmassive = massive.Split(' ');
+                if (splitmassive.Length < count)
+                {
+                    ConsoleTextBox.Text += Environment.NewLine;
+                    ConsoleTextBox.Text += "Вы ввели недостаточно чисел введите их сново";
+                    massive = String.Empty;
+                    InputTextBox.Text = String.Empty;
+                }
+                else
+                {
+                    string[] koord = new string[2];
+                    InputTextBox.Text = String.Empty;
+                    for (int i = 0; i < count; i++)
+                    {
+                        ddmassive[i] = new double[2];
+                        koord = splitmassive[i].Split(';');
+                        ddmassive[i][0] = Convert.ToDouble(koord[0]);
+                        ddmassive[i][1] = Convert.ToDouble(koord[1]);
+                    }
+                }
+                ConsoleTextBox.Text += Environment.NewLine;
+                ConsoleTextBox.Text += String.Join(Environment.NewLine, clusterist.ClusteringTestApp(ddmassive));
+            }
+            if (count == 0)
+            {
+                count = Convert.ToInt32(InputTextBox.Text);
+                ConsoleTextBox.Text += Environment.NewLine;
+                ConsoleTextBox.Text += "Введите " + count.ToString() + " координат через пробел. Пример x;y";
+                InputTextBox.Text = String.Empty;
+                ddmassive = new double[count][];
+            }
+        }
+
+        private void odniceMass()
+        {
+            if (massive == String.Empty && count != 0)
+            {
+                string[] splitmassive;
+
+                ConsoleTextBox.Text += Environment.NewLine;
+                ConsoleTextBox.Text += InputTextBox.Text;
+
+                massive = InputTextBox.Text;
+                massive = massive.Replace('.', ',');
+
+                splitmassive = massive.Split(' ');
+                if (splitmassive.Length < count)
+                {
+                    ConsoleTextBox.Text += Environment.NewLine;
+                    ConsoleTextBox.Text += "Вы ввели недостаточно чисел введите их сново";
+                    massive = String.Empty;
+                    InputTextBox.Text = String.Empty;
+                }
+                else
+                {
+                    InputTextBox.Text = String.Empty;
+                    for (int i = 0; i < count; i++)
+                    {
+                        dmassive[i] = Convert.ToDouble(splitmassive[i]);
+                    }
+                }
+                ConsoleTextBox.Text += Environment.NewLine;
+                ConsoleTextBox.Text += String.Join(Environment.NewLine, clusterist.ClusteringTestApp(dmassive));
+            }
+            if (count == 0)
+            {
+                count = Convert.ToInt32(InputTextBox.Text);
+                ConsoleTextBox.Text += Environment.NewLine;
+                ConsoleTextBox.Text += "Введите " + count.ToString() + " чисел через пробел";
+                InputTextBox.Text = String.Empty;
+                dmassive = new double[count];
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ConsoleTextBox.Text = String.Empty;
+            count = 0;
+            massive = String.Empty;
+            ConsoleTextBox.Text += "Введите количество точек";
+        }
+    }
+}
